4c6a37727cc896e67ae95e0d804451ebff98317e
GG <giagullo@gmail.com>
2018-12-30 17:47:11 +0100

    first local modification to base project

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var result = require('./routes/result');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/result', result);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
